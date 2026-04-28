99d99543befc9fe22d9e85dedad8a6df93fc13c2
BnL-FaTSo <jtk5768@psu.edu>
2026-04-28 19:28:10 -0400

    Add files via upload

--- TicTacToe.py
+++ TicTacToe.py
@@ -6,13 +6,20 @@
 
 class TicTacToe:
     def __init__(self):
+        """
+            The initializer method for the tic-tac-toe game, also coincidentally starts the game
+        """
         self.board = [[" ", " ", " "],
                       [" ", " ", " "],
                       [" ", " ", " "]]
         self.xTurn = True
+        self.wasOpen = True
         self.takeTurn()
 
     def takeTurn(self):
+        """
+            Checks which turn it is and calls move() for that player
+        """
         if self.xTurn:
             self.xTurn = False
             self.move("X")
@@ -21,91 +28,111 @@ class TicTacToe:
             self.move("O")
 
     def move(self, turn):
-        self.printBoard()
-        print(f"It is {turn}s turn. Please input which row you want to move in. (input Quit to quit)")
+        """
+            Asks for the coordinates of the move, using other methods to check errors, wins, draws, open, and acts accordingly
+        """
+        if self.wasOpen: self.printBoard()
+        else: self.wasOpen = True
+        print(f"\nIt is {turn}s turn. Please input which row you want to move in. (input Quit to quit)\n")
         rowMove = self.errorCheck()
         if rowMove == "Quit":
-            print("Thank you for playing.")
+            print("\nThank you for playing.\n")
             return
-        print("Now please select a column. (input Quit to quit)")
+        print("\nNow please select a column. (input Quit to quit)\n")
         colMove = self.errorCheck()
-        if rowMove == "Quit":
-            print("Thank you for playing.")
+        if colMove == "Quit":
+            print("\nThank you for playing.\n")
             return
         if self.open(rowMove, colMove):
             self.board[rowMove][colMove] = turn
             if not self.winCondition() and not self.drawCondition():
                 self.takeTurn()
         else:
-            print("That spot is taken.")
+            print("\nThat spot is taken.")
+            self.wasOpen = False
             self.move(turn)
 
     def errorCheck(self):
+        """
+            Checks if the input is one of the numbers or Quit, recursively correcting it
+        """
         move = input()
         if move == "Quit": return move
         elif len(move) > 1: 
-            print("Please input one character. (input Quit to quit)")
+            print("\nPlease input one character. (input Quit to quit)\n")
             return self.errorCheck()
         elif not move.isnumeric():
-            print("Please input a number. (input Quit to quit)")
+            print("\nPlease input a number. (input Quit to quit)\n")
             return self.errorCheck()
         elif 0 > int(move) or 2 < int(move):
-            print("Please input one of the numbers in the board. (input Quit to quit)")
+            print("\nPlease input one of the numbers that has a corresponding spot on the board. (input Quit to quit)\n")
             return self.errorCheck()
         else: return int(move)
         
     def open(self, row, col):
+        """
+            Checks if a given spot is open
+        """
         if self.board[row][col] == " ": return True
         else: return False
 
     def winCondition(self):
+        """
+            Checks each of the 8 win conditions and prints the winner if there is one
+        """
         if " " != self.board[0][0] == self.board[1][0] == self.board[2][0]:
             self.printBoard()
-            print(f"{self.board[0][0]}s win.")
+            print(f"\n{self.board[0][0]}s win.\n")
             return True
         elif " " != self.board[0][0] == self.board[1][1] == self.board[2][2]:
             self.printBoard()
-            print(f"{self.board[0][0]}s win.")
+            print(f"\n{self.board[0][0]}s win.\n")
             return True
         elif " " != self.board[0][0] == self.board[1][0] == self.board[2][0]:
             self.printBoard()
-            print(f"{self.board[0][0]}s win.")
+            print(f"\n{self.board[0][0]}s win.\n")
             return True
         elif " " != self.board[1][0] == self.board[1][1] == self.board[1][2]:
             self.printBoard()
-            print(f"{self.board[1][0]}s win.")
+            print(f"\n{self.board[1][0]}s win.\n")
             return True
         elif " " != self.board[2][0] == self.board[2][1] == self.board[2][2]:
             self.printBoard()
-            print(f"{self.board[2][0]}s win.")
+            print(f"\n{self.board[2][0]}s win.\n")
             return True
         elif " " != self.board[2][0] == self.board[1][1] == self.board[0][2]:
             self.printBoard()
-            print(f"{self.board[2][0]}s win.")
+            print(f"\n{self.board[2][0]}s win.\n")
             return True
         elif " " != self.board[0][1] == self.board[1][1] == self.board[2][1]:
             self.printBoard()
-            print(f"{self.board[0][1]}s win.")
+            print(f"\n{self.board[0][1]}s win.\n")
             return True
         elif " " != self.board[0][2] == self.board[1][2] == self.board[2][2]:
             self.printBoard()
-            print(f"{self.board[0][2]}s win.")
+            print(f"\n{self.board[0][2]}s win.\n")
             return True
         else: return False
     
     def drawCondition(self):
+        """
+            Checks if the game is a draw
+        """
         for row in self.board:
             for col in row:
                 if col == " ":
                     return False
-        if self.winCondition():
-            return False
-        self.printBoard()
-        print("You tied.")
-        return True
+        if self.winCondition(): return False
+        else:
+            self.printBoard()
+            print("\nYou tied.\n")
+            return True
     
     def printBoard(self):
-        print(f"    0    1    2\n0 {self.board[0]}\n\n1 {self.board[1]}\n\n2 {self.board[2]}")
+        """
+            Prints a legible form of the board
+        """
+        print(f"\n    0    1    2\n0 {self.board[0]}\n\n1 {self.board[1]}\n\n2 {self.board[2]}")
             
 def main():
     TicTacToe()
